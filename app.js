62809beaa930bd9a3158abbe6626cdc6ca1dfc90
kousikdasaws <kousik.das.aws@gmail.com>
2024-12-08 01:38:13 -0500

    12-8-2024-commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
+const app = express();
 
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
